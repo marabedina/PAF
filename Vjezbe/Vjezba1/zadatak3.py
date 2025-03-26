69bf0ee2d398b6b1936a0315a15db555994183af
unknown <mbedina@pmfst.hr>
2025-03-26 19:40:04 +0100

    zadatak 3

---
+++ Vjezbe/Vjezba1/zadatak3.py
@@ -0,0 +1,10 @@
+x1=float(input("Unesite x koordinattu prve točke"))
+y1=float(input("Unesite y koordinatu prve točke "))
+x2=float(input("Unesite x koordinatu druge točke "))
+y2=float(input("Unesite y koordinatu druge točke "))
+print("prva tocka: ({},{})".format(x1,y1))
+print("prva tocka: ({},{})".format(x2,y2))
+k=(y2-y1)/(x2-x1)
+l=y1-k*x1
+print("jednadzba pravca:")
+print("y={}x+{}".format(k,l))
